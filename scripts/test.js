0b8c514094a6895e6d35f68bc4ac9f457d8022be
topkoong <engineer_top@msn.com>
2016-11-07 00:07:16 -0500

    Updated

--- scripts/test.js
+++ scripts/test.js
@@ -3,7 +3,16 @@ var suggests = ["align", "goals", "successful", "resources", "capitalize", "enga
 "solutions", "efficiency", "global marketplace", "international", "cross-cultural", "communication", "manage",
 "distill", "monitor", "outsource", "function", "growing", "potential", "wellness-oriented", "specialized",
 "specializing", "healthy", "healthier", "health", "niche", "agile", "market driven opportunity", "problem", "focus",
-"approach", "Vision Statement", "Mission Statement", "solution", "resolve"];
+"approach", "Vision Statement", "Mission Statement", "solution", "resolve", "alternatives", "Competition",
+"competitive edge", "competitive edges", "competitive landscape", "competitive space", "Competitors", "decline",
+"direct competition", "direct competitor", "domestic", "expansion", "figures", "foreign", "growth",
+"indirect competition", "indirect competitor", "keys to success", "Major players", "NAICS code",
+"research", "substitute", "success factors", "will compete in", "sustainable competitive advantage",
+"key success factors", "fragmented", "dominant", "scattered", "monopoly", "oligopoly", "duopoly",
+"growth life cycle", "technology", "barriers to entry", "threat of substitutes", "buyer power",
+"supplier power", "competitive rivalry", "threat of entry", "critical outcomes", "emerging",
+"next best alternative", "NBA", "source of growth", "mature stage of growth", "leisure activities",
+"client base", "boost demand", "decline", "proves favorable", "per capita"];
     $("#area5").asuggest(suggests, {
         'endingSymbols': ' ',
         'stopSuggestionKeys': [$.asuggestKeys.RETURN],
