8c7dc77a2416e1ded7483eb31b0fc45f25441b62
topkoong <engineer_top@msn.com>
2016-12-01 22:55:39 -0500

    Add importedData 2

--- importeddata/scripts/test.js
+++ importeddata/scripts/test.js
@@ -1,27 +1,69 @@
-var xmlhttp = new XMLHttpRequest();
-var url = "file://Users/toppy/Downloads/topkoong.github.io/importeddata/scripts/vocablistfull.json";
+$(document).ready(function() {
 
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status === 200) {
-        var myArr = JSON.parse(this.responseText);
-        myFunction(myArr);
-    }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+            // $("#driver").click(function(event){
+            //    $.getJSON('scripts/test.json', function(jd) {
+            //       $('#stage').html('<p> Name: ' + jd.name + '</p>');
+            //       $('#stage').append('<p>Age : ' + jd.age+ '</p>');
+            //       $('#stage').append('<p> Sex: ' + jd.sex+ '</p>');
+            //    });
+            // });
 
-function myFunction(data) {
-    //var out = "";
-    var i;
-    for(i = 0; i < arr.length; i++) {
-      tr = $("<tr/>");
-      tr.append("<td>" + data[i].id+ "</td>");
-      tr.append("<td>" + data[i].latin + "</td>");
-      tr.append("<td>" + data[i].lesson + "</td>");
-      tr.append("<td>" + data[i].type + "</td>");
-      tr.append("<td>" + data[i].derivative + "</td>");
-      tr.append("<td>" + data[i].tense + "</td>");
-      $('table').append(tr);
-    }
-    document.getElementById("id01").innerHTML = out;
-}
+              $.getJSON('scripts/vocablistfull.json', function(data) {
+                              // Loop through data.report instead of data
+                for (var i = 0; i < data.length; i++) {
+                  var tr = $('<tr/>');
+                  $(tr).append("<td>" + data[i].id + "</td>");
+                  $(tr).append("<td>" + data[i].latin + "</td>");
+                  $(tr).append("<td>" + data[i].lesson + "</td>");
+                  $(tr).append("<td>" + data[i].type + "</td>");
+                  $(tr).append("<td>" + data[i].english+ "</td>");
+                  $(tr).append("<td>" + data[i].derivative + "</td>");
+                  $(tr).append("<td>" + data[i].sentence + "</td>");
+                  $(tr).append("<td>" + data[i].tense + "</td>");
+                  $('.rwd-table').append(tr);
+                }
+              });
+
+}); // end of ready function
+//
+// $(document).ready(function() {
+//   // Using the JSON format from your question
+//   // var data = {
+//   //   "report": [{
+//   //       "Mes": "Abril",
+//   //       "Dia": "13",
+//   //       "Local": "Lisboa",
+//
+//   $.getJSON('result.json', function(jd) {
+//                  $('#stage').html('<p> Name: ' + jd.name + '</p>');
+//                  $('#stage').append('<p>Age : ' + jd.age+ '</p>');
+//                  $('#stage').append('<p> Sex: ' + jd.sex+ '</p>');
+//   });
+//   //
+//   //   }]
+//   $.getJSON("vocablistfull.json", function(data) {
+//                   // Loop through data.report instead of data
+//                   for (var i = 0; i < data.length; i++) {
+//                     var tr = $('<tr/>');
+//                     // Indexing into data.report for each td element
+//                     $(tr).append("<td>" + data.report[i].id + "</td>");
+//                     $(tr).append("<td>" + data.report[i].latin + "</td>");
+//                     $(tr).append("<td>" + data.report[i].lesson + "</td>");
+//                     $('.table1').append(tr);
+//                   }
+//   });
+//   //};
+//
+// });
+//
+//
+//
+//
+//         // var tr = $("<tr/>");
+//         // tr.append("<td>" + data[i].id+ "</td>");
+//         // tr.append("<td>" + data[i].latin + "</td>");
+//         // tr.append("<td>" + data[i].lesson + "</td>");
+//         // tr.append("<td>" + data[i].type + "</td>");
+//         // tr.append("<td>" + data[i].derivative + "</td>");
+//         // tr.append("<td>" + data[i].tense + "</td>");
+//         // $('.id01').append(tr);
